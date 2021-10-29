0b80976b5b0661645a7ad4bec7a190fe2143e15e
Tarikul Islam <tarikul47.com>
2021-10-29 12:57:46 +0600

    server test

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-// EXRESS import 
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-const port = process.env.PORT||5000;
+const port = 5000;
 
-// ROUT API 
-app.get('/', (req, res) => {
-    res.send('Tourism server Runing');
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+// dbUser
+// tJRyuWBhr3i5FGL1
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dyvua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// console.log(uri);
+
+async function run(){
+    try{
+        await client.connect();
+        console.log('Conected to database');
+        const database = client.db("tourism");
+        const servicesCollection = database.collection("services");
+
+        // GET API
+        app.get('/', (req, res)=> {
+            res.send('database connected by api');
+        });
+
+    }
+    finally{
+       // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res)=> {
+    res.send('Running Genius Service');
 });
 
-app.get('/hello', (req, res) => {
-    res.send('hello! How are you?');
-});
-
-// SERVER Listening
 app.listen(port, () => {
-    console.log("Yes, Tourism server Runing", port);
+    console.log('Running Genius Server on', port);
 });
