3cae6ca18c0b1b077f7edde0cbb4609c178b654e
Tarikul Islam <tarikul47@gmail.com>
2021-10-29 14:19:02 +0600

    server test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors());
@@ -40,9 +40,9 @@ async function run(){
 run().catch(console.dir);
 
 app.get('/', (req, res)=> {
-    res.send('Running Genius Service');
+    res.send('Yes, Tourism Server is running.');
 });
 
 app.listen(port, () => {
-    console.log('Running Genius Server on', port);
+    console.log('Yes, Tourism Server is running.', port);
 });
