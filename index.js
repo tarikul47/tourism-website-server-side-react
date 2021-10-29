6b54077870d35cd38d53f8e62cd84e90110909d9
Tarikul Islam <tarikul47.com>
2021-10-29 12:34:08 +0600

    server test

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.use(express.json());
 // tJRyuWBhr3i5FGL1
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dyvua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dyvua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+//const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 //console.log(uri);
 
 /*
